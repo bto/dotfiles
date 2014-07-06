603b429775c1bc1f6df18ae21f035d4c54fca272
Masato Bito <masato@bz2.jp>
2014-07-07 03:34:37 +0900

    rename

--- etc/zsh/.zshrc
+++ etc/zsh/.zshrc
@@ -1,3 +1,3 @@
-_glob_source "$ZDOTDIR/rc.d/*"
+_glob_source "$ZDOTDIR/rc.d/*.zsh"
 _glob_source '~/.zsh?*/.zshrc'
 _glob_source "~/.zsh?*/rc.d/*"
--- etc/zsh/rc.d/prompt.zsh
+++ etc/zsh/rc.d/prompt.zsh
@@ -1,3 +1,2 @@
-# prompt
 PROMPT='%{[$[31+$RANDOM % 6]m%}%B%U%m'"{%n}%#%{[m%}%u%b "
 RPROMPT='%{[$[31+$RANDOM % 6]m%}%B[%(?.%h.ERROR:%?)] %D{%m/%d %R} [%3c]%{[m%}%b'
