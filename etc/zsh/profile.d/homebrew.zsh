9c8ec0033272801ddfe4182b8207579b998736a9
Masato Bito <masato@bz2.jp>
2014-07-07 03:04:30 +0900

    rename

--- etc/zsh/profile.d/homebrew.zsh
+++ etc/zsh/profile.d/homebrew.zsh
@@ -1,9 +1,6 @@
 _command_exists brew || return
 
-# coreutils
-_glob_add_path_env PATH $(brew --prefix coreutils)/libexec/gnubin
-_glob_add_path_env MANPATH $(brew --prefix coreutils)/libexec/gnuman
+_glob_add_path_env PATH $(brew --prefix)/opt/*/libexec/gnubin
+_glob_add_path_env MANPATH $(brew --prefix)/opt/*/libexec/gnuman
 
-# gnu-sed
-_glob_add_path_env PATH $(brew --prefix gnu-sed)/libexec/gnubin
-_glob_add_path_env MANPATH $(brew --prefix gnu-sed)/libexec/gnuman
+_glob_source "$ZDOTDIR/profile.d/homebrew.d/*.zsh"
