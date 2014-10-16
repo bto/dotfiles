77c0866bab57d326de8c57b54ea8827cb65d5486
Masato Bito <masato@bz2.jp>
2014-10-16 10:22:58 +0900

    fix keychain

--- rc/zsh/profile.d/keychain.zsh
+++ rc/zsh/profile.d/keychain.zsh
@@ -1,6 +1,12 @@
 _command_exists keychain || return
 
-keychain id_rsa id_dsa
+if [ -f $HOME/.ssh/id_rsa ]; then
+    keychain id_rsa
+fi
+
+if [ -f $HOME/.ssh/id_dsa ]; then
+    keychain id_dsa
+fi
 
 if [ -f $HOME/.keychain/$HOST-sh ]; then
     source $HOME/.keychain/$HOST-sh
