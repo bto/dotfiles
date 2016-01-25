620360c2b6bcc7d28f28a33c660b1e74104d5dfd
Masato Bito <masato@bz2.jp>
2016-01-25 15:49:03 +0900

    remove ssh-config.d task

--- config/Makefile.d/ssh-config.d/task.mk
+++
@@ -1,9 +0,0 @@
-.PHONY: ssh-config.d-install
-ssh-config.d-install: $(SSH_CONFIG_D_DIR)
-
-.PHONY: ssh-config.d-update
-ssh-config.d-update: $(SSH_CONFIG_D_DIR)
-	cd $< && git fetch -p && git pull
-
-$(SSH_CONFIG_D_DIR):
-	git clone -b $(SSH_CONFIG_D_BRANCH) $(SSH_CONFIG_D_REPO) $@
--- config/Makefile.d/ssh-config.d/var.mk
+++
@@ -1,6 +0,0 @@
-SSH_CONFIG_D_DIR = ~/.ssh/config.d
-SSH_CONFIG_D_REPO = git@bitbucket.org:btomasato/.ssh.git
-SSH_CONFIG_D_BRANCH = config.d
-
-INSTALL_TARGETS += ssh-config.d-install
-UPDATE_TARGETS += ssh-config.d-update
