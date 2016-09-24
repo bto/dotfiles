b6fbfb7d9705d4888b1c3bf8cfeeb07fc99a3197
Masato Bito <masato@bz2.jp>
2016-09-24 16:58:22 +0900

    add vim/Makefile

--- config/Makefile.d/vim/var.mk
+++
@@ -1,9 +0,0 @@
-VIM_DIR = ~/.vim
-VIM_PLUGIN_DIR = $(VIM_DIR)/plugin
-
-VIM_EDITORCONFIG = $(VIM_PLUGIN_DIR)/editorconfig.vim
-VIM_EDITORCONFIG_URL = https://github.com/editorconfig/editorconfig-vim/archive/v0.3.2.tar.gz
-VIM_EDITORCONFIG_PKG = $(VIM_DIR)/editorconfig-vim-0.3.2.tar.gz
-VIM_EDITORCONFIG_DIR = $(VIM_DIR)/editorconfig-vim-0.3.2
-
-INSTALL_TARGETS += vim-install
