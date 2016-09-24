12cfd7ca40e41c2a6fff9cdd67516950a752aade
Masato Bito <masato@bz2.jp>
2016-09-24 17:15:07 +0900

    add zsh-syntax-hilighting module

--- config/Makefile.d/zsh-syntax-highlighting/var.mk
+++
@@ -1,5 +0,0 @@
-ZSH_SYNTAX_HIGHLIGHTING_DIR = $(RC_DIR)/zsh/vendor/zsh-syntax-highlighting
-ZSH_SYNTAX_HIGHLIGHTING_REPO = git@github.com:zsh-users/zsh-syntax-highlighting.git
-
-INSTALL_TARGETS += zsh-syntax-highlighting-install
-UPDATE_TARGETS += zsh-syntax-highlighting-update
