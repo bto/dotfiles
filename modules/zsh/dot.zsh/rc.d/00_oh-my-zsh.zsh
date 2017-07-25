f88b8ff89f6876bf9ce37178d330cc144f42b6c5
Masato Bito <masato@bz2.jp>
2017-07-25 15:03:07 +0900

    improve zsh.rc

--- modules/zsh/dot.zsh/rc.d/00_oh-my-zsh.zsh
+++ modules/zsh/dot.zsh/rc.d/00_oh-my-zsh.zsh
@@ -1,11 +1,1 @@
-[ -d $HOME/.oh-my-zsh ] || return
-
-export ZSH=$HOME/.oh-my-zsh
-
-ZSH_THEME="robbyrussell"
-
-plugins=(git themes)
-
-source $ZSH/oh-my-zsh.sh
-
-unset LC_CTYPE
+_sources $DOTFILES_DIR/modules/oh-my-zsh/zsh.rc.init(N-.)
---
+++ modules/zsh/dot.zsh/rc.d/01_langenv.zsh
@@ -0,0 +1,1 @@
+_sources $DOTFILES_DIR/modules/*env/zsh.rc.init(N-.)
