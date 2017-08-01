b1b09a9937658026edd32756e5339e95b7dbf3a8
Masato Bito <masato@bz2.jp>
2017-08-01 10:06:49 +0900

    add git branch to prompt

--- modules/zsh/dot.zsh/rc.d/prompt.zsh
+++ modules/zsh/dot.zsh/rc.d/prompt.zsh
@@ -1,12 +1,20 @@
 setopt transient_rprompt
 setopt prompt_subst
 
-function _set_prompt() {
+function _prompt_git_branch() {
+    branch=$(git branch 2> /dev/null | grep '^*' | cut -d ' ' -f 2)
+    if [ x$branch = 'x' ]; then
+        return
+    fi
+    echo "(git:$branch)"
+}
+
+function _prompt_set() {
     local random_color_start=$'%{\e[$[31+$RANDOM%6]m%}'
     local random_color_end=$'%{\e[m%}'
 
-    PROMPT=$random_color_start$'%B%0(?||[ERROR:%?] )[%D{%T}](%~)\n%#%b '$random_color_end
+    PROMPT=$random_color_start$'%B%0(?||[ERROR:%?] )[%D{%T}]$(_prompt_git_branch)(%~)\n%#%b '$random_color_end
     RPROMPT=$random_color_start"%n@%m %D{%m/%d}"$random_color_end
 }
 
-_set_prompt
+_prompt_set
