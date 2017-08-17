e98d64d43c7ef489304393eb6a1e1e03c8da96a1
Masato Bito <masato@bz2.jp>
2017-08-17 12:27:55 +0900

    change peco layout

--- modules/peco/rc.d/cd.zsh
+++ modules/peco/rc.d/cd.zsh
@@ -1,6 +1,6 @@
 function jcd()
 {
-    local dir=$(_z -j 2>&1 | tac | peco --query "$*" | awk '{print $2}')
+    local dir=$(_z -j 2>&1 | tac | peco --layout=bottom-up --query "$*" | awk '{print $2}')
     if [ ! -z "$dir" ]; then
         cd $dir
     fi
--- modules/peco/rc.d/history.zsh
+++ modules/peco/rc.d/history.zsh
@@ -1,6 +1,6 @@
 function _peco_select_history()
 {
-    BUFFER=$(fc -l -n 1 | tac | peco --query "$LBUFFER")
+    BUFFER=$(fc -l -n 1 | tac | peco --layout=bottom-up --query "$LBUFFER")
     CURSOR=$#BUFFER         # move cursor
     zle -R -c               # refresh
 }
