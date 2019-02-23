d89b1bf0125022faa80012256bb6e92c5c0cbeba
Masato Bito <masato@bz2.jp>
2019-02-23 19:53:09 +0900

    remove /usr/local/opt from path environments

--- modules/zsh/dot.zsh/profile.d/path.d/usr_local.zsh
+++ modules/zsh/dot.zsh/profile.d/path.d/usr_local.zsh
@@ -1,8 +1,6 @@
 typeset -U tmp_path
 
 tmp_path=(
-    /usr/local/opt/*/include(N-/)
-    /usr/local/opt/include(N-/)
     /usr/local/*/include(N-/)
     /usr/local/include(N-/)
 )
@@ -10,8 +8,6 @@ c_include_path=($tmp_path $c_include_path)
 cplus_include_path=($tmp_path $cplus_include_path)
 
 tmp_path=(
-    /usr/local/opt/*/lib(N-/)
-    /usr/local/opt/lib(N-/)
     /usr/local/*/lib(N-/)
     /usr/local/lib(N-/)
 )
@@ -19,14 +15,6 @@ ld_library_path=($tmp_path $ld_library_path)
 library_path=($tmp_path $library_path)
 
 manpath=(
-    /usr/local/opt/*/share/man/ja(N-/)
-    /usr/local/opt/*/share/man(N-/)
-    /usr/local/opt/*/man/ja(N-/)
-    /usr/local/opt/*/man(N-/)
-    /usr/local/opt/share/man/ja(N-/)
-    /usr/local/opt/share/man(N-/)
-    /usr/local/opt/man/ja(N-/)
-    /usr/local/opt/man(N-/)
     /usr/local/*/share/man/ja(N-/)
     /usr/local/*/share/man(N-/)
     /usr/local/*/man/ja(N-/)
@@ -39,10 +27,6 @@ manpath=(
 )
 
 path=(
-    /usr/local/opt/*/sbin(N-/)
-    /usr/local/opt/*/bin(N-/)
-    /usr/local/opt/sbin(N-/)
-    /usr/local/opt/bin(N-/)
     /usr/local/*/sbin(N-/)
     /usr/local/*/bin(N-/)
     /usr/local/sbin(N-/)
@@ -51,8 +35,6 @@ path=(
 )
 
 pkg_config_path=(
-    /usr/local/opt/*/lib/pkgconfig(N-/)
-    /usr/local/opt/lib/pkgconfig(N-/)
     /usr/local/*/lib/pkgconfig(N-/)
     /usr/local/lib/pkgconfig(N-/)
     $pkg_config_path
