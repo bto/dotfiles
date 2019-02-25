ebc711002b8bd0b5fa228d6cfc763a01f393a74f
Masato Bito <masato@bz2.jp>
2019-02-25 11:53:25 +0900

    add zshrc.post.d

--- src/zsh/zshrc.zsh
+++ src/zsh/zshrc.zsh
@@ -1,4 +1,5 @@
 _sources $DOTFILES_SOURCE_DIR/zsh/zshrc.d/*.zsh(N-.)
 _sources $DOTFILES_DIR/modules/*/zshrc.zsh(N-.)
+_sources $DOTFILES_SOURCE_DIR/zsh/zshrc.post.d/*.zsh(N-.)
 _sources $ZDOTDIR/zshrc.zsh(N-.)
 _sources $ZDOTDIR/zshrc.d/*.zsh(N-.)
